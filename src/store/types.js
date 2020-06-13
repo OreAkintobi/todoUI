f37cdc19ed279622e165c8c9b44a2fa760ede34b
OreAkintobi <oreakintobi@gmail.com>
2020-06-13 17:32:58 +0100

    added store to App

---
+++ src/store/types.js
@@ -0,0 +1,9 @@
+import { createContext } from "react";
+
+// import all reducers on store
+
+// import all reducers initial state interface
+
+const StoreContext = createContext();
+
+export default StoreContext;
