c9fa903e75635cc20c3483234491024a2d0871b3
OreAkintobi <oreakintobi@gmail.com>
2020-06-13 18:12:23 +0100

    create initial login and signup screens

---
+++ src/screens/loginscreen/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SafeAreaView from "../../commons/safe-area-view";
+import { Container, MidText } from "./styles";
+
+const LoginScreen = () => {
+  return (
+    <SafeAreaView>
+      <Container>
+        <MidText>This is the Login Screen.</MidText>
+      </Container>
+    </SafeAreaView>
+  );
+};
+
+export default LoginScreen;
---
+++ src/screens/signupscreen/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SafeAreaView from "../../commons/safe-area-view";
+import { Container, MidText } from "./styles";
+
+const SignupScreen = () => {
+  return (
+    <SafeAreaView>
+      <Container>
+        <MidText>This is the Signup Screen.</MidText>
+      </Container>
+    </SafeAreaView>
+  );
+};
+
+export default SignupScreen;
