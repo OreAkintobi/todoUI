fd0e100c8b8499d461d314b4f01b8ac36f347081
OreAkintobi <oreakintobi@gmail.com>
2020-06-13 02:15:06 +0100

    added fonts to assets and exposed to App.js

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useFonts } from "@use-expo/font";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { AppLoading } from "expo";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+export default () => {
+  let [fontsLoaded] = useFonts({
+    "Roboto-Black": require("./assets/fonts/Roboto-Black.ttf"),
+    "Roboto-BlackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-BoldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
+    "Roboto-Italic": require("./assets/fonts/Roboto-Italic.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-LightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-MediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Thin": require("./assets/fonts/Roboto-Thin.ttf"),
+    "Roboto-ThinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>
+          Open up App.js to start working on your app!
+        </Text>
+      </View>
+    );
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "Roboto-Black",
   },
 });
