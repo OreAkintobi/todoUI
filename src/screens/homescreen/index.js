760b37febd83d99e809c682048ec3105b763570c
OreAkintobi <oreakintobi@gmail.com>
2020-06-14 08:29:58 +0100

    added wallet component and styled homescreen

--- src/screens/homescreen/index.js
+++ src/screens/homescreen/index.js
@@ -1,18 +1,51 @@
 import React from "react";
-import { Button } from "react-native";
+import { ScrollView } from "react-native";
 import SafeAreaView from "../../commons/safe-area-view";
-import { Container, MidText } from "./styles";
+import WalletBalance from "../../components/WalletBalance";
+
+import {
+  Container,
+  ActivitiesContainer,
+  Activity,
+  ActivityCaption,
+  ActivitySelector,
+  AdvertSpace,
+  AdvertText,
+} from "./styles";
+
+const activityItems = [
+  "wallet",
+  "buy data",
+  "buy airtime",
+  "wallet history",
+  "pricing",
+  "contact",
+];
 
 const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
-      <Container>
-        <MidText>This is the Home Screen.</MidText>
-        <Button
-          title="Go to Airtime Screen"
-          onPress={() => navigation.navigate("AirtimeScreen")}
-        />
-      </Container>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          flexGrow: 1,
+        }}
+      >
+        <Container>
+          <WalletBalance />
+          <ActivitiesContainer>
+            {activityItems.map((item) => (
+              <Activity key={item}>
+                <ActivitySelector />
+                <ActivityCaption>{item}</ActivityCaption>
+              </Activity>
+            ))}
+          </ActivitiesContainer>
+          <AdvertSpace>
+            <AdvertText>Advert here</AdvertText>
+          </AdvertSpace>
+        </Container>
+      </ScrollView>
     </SafeAreaView>
   );
 };
--- src/screens/homescreen/styles.js
+++ src/screens/homescreen/styles.js
@@ -3,10 +3,64 @@ import { theme } from "../../theme/types";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   background-color: ${theme.colors.COLOR_WHITE};
 `;
-export const MidText = styled.Text`
-  font-family: ${theme.fonts.RobotoBlackItalic};
+
+export const ActivitiesContainer = styled.View`
+  margin-top: 30px;
+  height: 224px;
+  width: 100%;
+  border-radius: 25px;
+  background-color: ${theme.colors.OFF_WHITE};
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  align-content: center;
+`;
+
+export const Activity = styled.TouchableOpacity`
+  width: 96px;
+  height: 83px;
+  margin-top: 5px;
+  margin-bottom: 5px;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ActivitySelector = styled.TouchableOpacity`
+  width: 56px;
+  height: 56px;
+  border-radius: 56px;
+  background-color: ${theme.colors.DARK_TEXT};
+`;
+
+export const ActivityCaption = styled.Text`
+  font-size: 12px;
+  line-height: 14px;
+  text-align: center;
+  text-transform: capitalize;
+  font-family: ${theme.fonts.RobotoRegular};
+  color: ${theme.colors.DARK_TEXT};
+`;
+
+export const AdvertSpace = styled.TouchableOpacity`
+  width: 90%;
+  height: 77px;
+  margin-top: 15px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${theme.colors.BG_LIGHTER_GRAY};
+`;
+
+export const AdvertText = styled.Text`
+  font-size: 16px;
+  line-height: 19px;
+  text-align: center;
+  text-transform: capitalize;
+  font-family: ${theme.fonts.RobotoRegular};
+  color: ${theme.colors.DARK_TEXT};
 `;
