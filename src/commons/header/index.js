f976b8a76f8912ff2ac0eba22283cb9ce8927b96
OreAkintobi <oreakintobi@gmail.com>
2020-06-16 02:04:00 +0100

    designed custom header component

---
+++ src/commons/header/index.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { theme } from "../../theme/types";
+import boxShadow from "../../utils/boxShadows";
+import BackButtonIcon from "../../../assets/icons/Small/BackButtonIcon";
+
+import {
+  Container,
+  HeaderLeft,
+  HeaderRight,
+  HeaderBackTitle,
+  HeaderName,
+  HeaderTitleContainer,
+  BackButtonTouchable,
+} from "./styles";
+
+export default function Header(props) {
+  const { colors } = theme;
+  const navigation = useNavigation();
+
+  const {
+    title,
+    style,
+    headerBackTitleVisible,
+    headerLeftContainerStyle,
+    headerRightContainerStyle,
+    functionPassDown,
+  } = props;
+
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor={colors.COLOR_WHITE} />
+      <Container
+        style={[
+          boxShadow({ elevation: 5, shadowColor: colors.DARK_TEXT }),
+          style,
+        ]}
+      >
+        {props.headerLeft ? (
+          <HeaderLeft style={headerLeftContainerStyle}>
+            <BackButtonTouchable
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <BackButtonIcon />
+            </BackButtonTouchable>
+            {headerBackTitleVisible && <HeaderBackTitle>back</HeaderBackTitle>}
+          </HeaderLeft>
+        ) : null}
+
+        <HeaderTitleContainer style={{ marginLeft: props.headerLeft ? 0 : 20 }}>
+          <HeaderName>{title}</HeaderName>
+        </HeaderTitleContainer>
+
+        <HeaderRight style={headerRightContainerStyle}>
+          {props.headerRight ? (
+            <BackButtonTouchable onPress={functionPassDown}>
+              <props.headerRight />
+            </BackButtonTouchable>
+          ) : null}
+        </HeaderRight>
+      </Container>
+    </>
+  );
+}
